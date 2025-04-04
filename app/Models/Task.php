da12c55327b394f23033d6e1f3b780eafa13ca64
Mikey <trueptimekhanoid@gmail.com>
2025-04-04 17:54:15 +0500

    PATCH: Error fixed

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'title',
         'description',
@@ -25,16 +22,20 @@ class Task extends Model
     ];
 
     protected $casts = [
-        'deadline' => 'datetime',
         'tags' => 'array',
-        'progress' => 'float',
+        'deadline' => 'datetime',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function userProgress()
     {
         return $this->hasMany(TaskUser::class);
     }
 
-    // Связь с текущим пользователем
     public function currentUserProgress()
     {
         return $this->hasOne(TaskUser::class)->where('user_id', auth()->id());
